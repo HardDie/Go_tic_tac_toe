ad197d5bd4955600cf2d001d5fa0d926f122a4f2
HardDie <oleg1995sysoev@yandex.ru>
2020-04-19 01:07:40 +0300

    Implement save/read to file functions

--- pool/pool.go
+++ pool/pool.go
@@ -1,8 +1,12 @@
 package pool
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"math/rand"
+	"os"
 	"tic_tac_toe/game"
 	"time"
 )
@@ -12,6 +16,8 @@ import (
  */
 
 const firstWeight = 3
+const constStudyWinWeight = 3
+const constStudyLoseWeight = 1
 
 /**
  * Types
@@ -19,8 +25,17 @@ const firstWeight = 3
 
 type variants [game.FieldWidth * game.FieldHeight]uint64
 
+type step struct {
+	field string
+	step  int
+}
+
 type Pool struct {
-	pool map[string]*variants
+	GameCounts  uint64
+	Width       uint16
+	Height      uint16
+	Pool        map[string]*variants
+	activeSteps []step
 }
 
 /**
@@ -29,7 +44,9 @@ type Pool struct {
 
 func NewPool() *Pool {
 	pool := Pool{}
-	pool.pool = make(map[string]*variants)
+	pool.Pool = make(map[string]*variants)
+	pool.Width = game.FieldWidth
+	pool.Height = game.FieldHeight
 	rand.Seed(time.Now().UnixNano())
 	return &pool
 }
@@ -38,31 +55,100 @@ func (p *Pool) GetStep(gm game.Game) (int, error) {
 	field := gm.Field2String()
 
 	// Init element if not exist
-	if _, ok := p.pool[field]; !ok {
-		p.pool[field] = &variants{}
+	if _, ok := p.Pool[field]; !ok {
+		p.Pool[field] = &variants{}
 		for i, val := range field {
 			if val == ' ' {
-				p.pool[field][i] = firstWeight
+				p.Pool[field][i] = firstWeight
 			}
 		}
 	}
 
 	// Calculate max random value
 	maxValue := uint64(0)
-	for _, val := range p.pool[field] {
+	for _, val := range p.Pool[field] {
 		maxValue += val
 	}
 
 	// Choose step
 	randValue := rand.Uint64() % maxValue
 	tmpValue := uint64(0)
-	for i, val := range p.pool[field] {
+	for i, val := range p.Pool[field] {
 		if (randValue >= tmpValue) &&
 			(randValue < tmpValue+val) {
+
+			p.activeSteps = append(p.activeSteps, step{field, i})
+
 			return i, nil
 		}
 		tmpValue += val
 	}
 
 	return 0, errors.New("Can't find step")
+}
+
+func (p Pool) WriteData(filename string) error {
+	dat, err := json.Marshal(p)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(filename, dat, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *Pool) ReadData(filename string) error {
+	dat, err := ioutil.ReadFile(filename)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// Skip reading if file not exist
+			return nil
+		}
+		return err
+	}
+
+	err = json.Unmarshal(dat, p)
+	if err != nil {
+		return err
+	}
+
+	if game.FieldWidth != p.Width ||
+		game.FieldHeight != p.Height {
+		return errors.New("Field size diffirent")
+	}
+
+	return nil
+}
+
+func (p *Pool) DoWin() {
+	for _, val := range p.activeSteps {
+		p.Pool[val.field][val.step] += constStudyWinWeight
+	}
+
+	p.GameCounts++
+}
+
+func (p *Pool) DoLose() {
+	for _, val := range p.activeSteps {
+		if p.Pool[val.field][val.step] > constStudyLoseWeight {
+			p.Pool[val.field][val.step] -= constStudyLoseWeight
+		} else {
+			p.Pool[val.field][val.step] = 1
+		}
+	}
+
+	p.GameCounts++
+}
+
+func (p Pool) Print() {
+	fmt.Println("GameCount:", p.GameCounts)
+	fmt.Println("Width:", p.Width)
+	fmt.Println("Height:", p.Height)
+	for key, val := range p.Pool {
+		fmt.Println(key, val)
+	}
 }
